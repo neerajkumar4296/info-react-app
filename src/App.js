2c9334e5379dac1695fe0de069d6f08e2bb0946f
neerajkumar4296 <60752969+neerajkumar4296@users.noreply.github.com>
2020-06-11 18:19:56 +0530

    code refactor

--- src/App.js
+++ src/App.js
@@ -1,80 +1,89 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import axios from "axios";
+import { properties } from './Components/properties'
 
-class App extends Component{
+class App extends Component {
 
-  constructor(props){
+  componentDidMount() {
+    document.title = "Corona Summary Tracker App"
+  }
+
+  constructor(props) {
     super(props)
-    
-    this.state={
-        country: '',
-        responseData:''
+
+    this.state = {
+      country: '',
+      responseData: '',
+      field: false
     }
-    this.handleChange=this.handleChange.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
   handleChange(event) {
-    this.setState({country: event.target.value});
- }
+    this.setState(
+      {
+        country: event.target.value
+      }
+    );
+  }
 
- fetchCasesSummary =  event => {
-    //alert('fetching report for country:: ' +this.state.country);
-
-    //const localApiurl='http://127.0.0.1:8090/utilities/coronaCases/summary/';
-    const devApiUrl='http://rest-info-service-dev1.ap-south-1.elasticbeanstalk.com/utilities/coronaCases/summary/';
+  fetchCasesSummary = event => {
 
     event.preventDefault()
 
-      axios.get(devApiUrl +this.state.country)
-      .then((response) =>
-     { 
-       console.log(response)
-       this.setState({
-        responseData: response.data
-      })
-    }
-    ).catch((error) => {
-      //console.log(error)
-      alert('Invalid Country Name:: ' +this.state.country)
-  })
-   
+    // reading constant values from properties file
+    let apiUrl = properties.coronaDevApiUrl
+
+    axios.get(apiUrl + this.state.country)
+      .then((response) => {
+        this.setState({
+          responseData: response.data,
+          field: true
+        })
+      }
+      ).catch(error => alert(error.message))
+
+
+
   }
 
-  
 
 
-  render(){
-  return (
-    <div className="App">
-      <header className="App-header">
-      <h2>Corona Virus Cases Summary</h2>
-      <form onSubmit={this.fetchCasesSummary}>
-                      <label>
-                        country: <input type="text" placeholder="eg: India" value={this.state.country}
-                        name="country" onChange={this.handleChange} />
-                      </label>
-                    <input type="submit" value="Submit" />
-                    </form>
-                   
-          <div  className="col">
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>Corona Virus Cases Summary</h2>
+          <form onSubmit={this.fetchCasesSummary}>
+            <label>
+              country: <input type="text" placeholder="eg: India" value={this.state.country}
+                name="country" disabled={this.state.field} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+
+          <div>
             <h3>Corona Virus Cases summary: {this.state.responseData.countryName}</h3>
             <p><b>total confirmed cases: {this.state.responseData.confirmed}</b></p>
-            <p><b>newly confirmed cases: {this.state.responseData.newlyConfirmed}</b></p>
-            <p><b>total deaths: {this.state.responseData.deaths}</b></p>
-            <p><b>new deaths: {this.state.responseData.newDeaths}</b></p>
             <p><b>total recovered: {this.state.responseData.recovered}</b></p>
+            <p><b>total active cases: {this.state.responseData.active}</b></p>
+            <p><b>total deaths: {this.state.responseData.deaths}</b></p>
+            <p><b>newly confirmed cases: {this.state.responseData.newlyConfirmed}</b></p>
             <p><b>newly recovered: {this.state.responseData.newlyRecovered}</b></p>
+            <p><b>new deaths: {this.state.responseData.newDeaths}</b></p>
             <p><b><i>last updated: {this.state.responseData.updatedTime}</i></b></p>
-          </div>       
-     
-      </header>
+          </div>
 
-       
+        </header>
+
+
       </div>
-  );
- }
-} 
+    );
+  }
+}
 
 
 
---
+++ src/Components/properties.js
@@ -0,0 +1,4 @@
+export const properties = {
+    coronalocalApiurl: 'http://127.0.0.1:8090/utilities/coronaCases/summary/',
+    coronaDevApiUrl: 'http://rest-info-service-dev1.ap-south-1.elasticbeanstalk.com/utilities/coronaCases/summary/'
+};
