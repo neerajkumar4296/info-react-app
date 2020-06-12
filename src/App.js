15c5e393e151164bbb6c77c378871fda35fe005e
neerajkumar4296 <60752969+neerajkumar4296@users.noreply.github.com>
2020-06-12 21:48:32 +0530

    changes for input fileds

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import { properties } from './Components/properties'
 
 class App extends Component {
 
-  componentDidMount() {
-    document.title = "Corona Summary Tracker App"
-  }
+  // componentDidMount() {
+  //   document.title = "Corona Summary Tracker App"
+  // }
 
   constructor(props) {
     super(props)
@@ -15,7 +15,9 @@ class App extends Component {
     this.state = {
       country: '',
       responseData: '',
-      field: false
+      disbleInputField: false,
+      hideResponseDiv: true,
+      hideResponseTable: true
     }
     this.handleChange = this.handleChange.bind(this)
   }
@@ -39,7 +41,9 @@ class App extends Component {
       .then((response) => {
         this.setState({
           responseData: response.data,
-          field: true
+          disbleInputField: true,
+          hideResponseDiv: false,
+          hideResponseTable: true
         })
       }
       ).catch(error => alert(error.message))
@@ -56,17 +60,77 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+        
           <h2>Corona Virus Cases Summary</h2>
           <form onSubmit={this.fetchCasesSummary}>
             <label>
               country: <input type="text" placeholder="eg: India" value={this.state.country}
-                name="country" disabled={this.state.field} onChange={this.handleChange} />
+                name="country" disabled={this.state.disbleInputField} onChange={this.handleChange} />
             </label>
             <input type="submit" value="Submit" />
           </form>
 
-          <div>
-            <h3>Corona Virus Cases summary: {this.state.responseData.countryName}</h3>
+          <form>
+            <div>
+              <label>opciones: </label>
+              <label>
+              <select>
+                <option value="india">India</option>
+                <option value="pakistan">Pakistan</option>
+                <option value="chad">Chad</option>
+                <option value="usa">United States of America</option>
+              </select>
+              
+              </label>
+              <input type="submit" disabled="true" value="Submit" />
+            </div>
+
+          </form>
+
+            <table id="casesSummary" hidden={this.state.hideResponseTable}>
+              <tr>
+              <th>Attribute</th>
+              <th>Count</th>
+              </tr>
+              <tr>
+              <tr>
+              <td>total confirmed cases</td>
+              <td>{this.state.responseData.confirmed}</td>
+              </tr>
+              <tr>
+              <td>total recovered</td>
+              <td>{this.state.responseData.recovered}</td>
+              </tr>
+              <tr>
+              <td>total active cases</td>
+              <td>{this.state.responseData.active}</td>
+              </tr>
+              <tr>
+              <td>total deaths</td>
+              <td>{this.state.responseData.deaths}</td>
+              </tr>
+              <tr>
+              <td>newly confirmed cases</td>
+              <td>{this.state.responseData.newlyConfirmed}</td>
+              </tr>
+              <tr>
+              <td>newly recovered</td>
+              <td>{this.state.responseData.newlyRecovered}</td>
+              </tr>
+              <tr>
+              <td>new deaths</td>
+              <td>{this.state.responseData.newDeaths}</td>
+              </tr>
+              <tr>
+              <td>last updated</td>
+              <td>{this.state.responseData.updatedTime}</td>
+              </tr>
+              </tr>
+            </table>
+
+
+          <div hidden={this.state.hideResponseDiv}>
+            <h4>Corona Virus Cases Summary: {this.state.responseData.countryName}</h4>
             <p><b>total confirmed cases: {this.state.responseData.confirmed}</b></p>
             <p><b>total recovered: {this.state.responseData.recovered}</b></p>
             <p><b>total active cases: {this.state.responseData.active}</b></p>
@@ -77,8 +141,8 @@ class App extends Component {
             <p><b><i>last updated: {this.state.responseData.updatedTime}</i></b></p>
           </div>
 
-        </header>
-
+        
+          </header>
 
       </div>
     );
